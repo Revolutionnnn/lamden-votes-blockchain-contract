046b5244311c9645122515bce73554aa57c21a0c
Michael Buritica <94705606+Revolutionnnn@users.noreply.github.com>
2023-08-10 22:56:38 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+proposal = Hash()
+ids = Variable()
+
+@construct
+def seed():
+    proposal['voteCount'] = 0
+    ids.set(0)
+
+@export
+def createProposal(name: str, proposalVote: str):
+    #ID 
+    int_id = ids.get() + 1
+    ids.set(int_id)
+
+    # CONTRUCTOR PROPOSALS
+    proposal['id'] = int_id
+    proposal['name'] = name
+    proposal['proposal'] = proposalVote
+    proposal['votes'] = 0
+    proposal['creator'] = ctx.caller
+
+    # TEXT
+    nombre = proposal['name'] = name
+    proposal_id = proposal['id']
+    proposal_pro = proposal['proposal']
+    votes = proposal['votes']
+    creator = proposal['creator']
+    Text = f'ID: {proposal_id}, NAME: {nombre}, PROPOSAL: {proposal_pro}, VOTES: {votes}, CREATOR: {creator}'
+    return Text
+
+
+
+
+
+
+    
